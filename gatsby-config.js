3031bbfb35851ae9508f748055552ab8796a134e
Oliver Fleckenstein <Oliverfl@live.dk>
2019-09-22 19:32:10 +0200

    Fix dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
@@ -41,13 +40,5 @@ module.exports = {
     // 'gatsby-plugin-typescript-checker',
     `gatsby-plugin-sass`,
     `gatsby-plugin-svgr`,
-    // {
-    //   resolve: `gatsby-plugin-react-svg`,
-    //   options: {
-    //     rule: {
-    //       include: /icons/
-    //     }
-    //   }
-    // }
   ],
 };
