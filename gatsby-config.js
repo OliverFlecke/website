2ca29d85b01cb8dae95a65e14809933a7d297e7e
Oliver Fleckenstein <Oliverfl@live.dk>
2019-09-22 20:03:33 +0200

    Undelete config file

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+  siteMetadata: {
+    title: `OliverFlecke`,
+    description: `Personal site for Oliver Fleckenstein`,
+    author: `Oliver Fleckenstein`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTsx: true,
+        transpileOnly: false,
+      },
+    },
+    // 'gatsby-plugin-typescript-checker',
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-svgr`,
+  ],
+};
